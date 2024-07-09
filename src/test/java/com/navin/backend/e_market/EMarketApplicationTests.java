b7deb6c0b448add0640b31e4496792aa71453580
Naveen Kumar <bnaveenk3@gmail.com>
2024-07-09 10:42:44 +0530

    project structur

---
+++ src/test/java/com/navin/backend/e_market/EMarketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.navin.backend.e_market;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EMarketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
